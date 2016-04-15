4f153755ad8cc571652c6180862fd9cce74f46d7
TheJP <jp_55555@hotmail.com>
2016-04-15 15:19:14 +0200

    Implemented camera control

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public float cameraSpeed;
+    public float rotation;
+
+    void Update()
+    {
+        //Camera movement
+        var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += Time.deltaTime * cameraSpeed * (Quaternion.AngleAxis(rotation, Vector3.up) * direction);
+    }
+}
