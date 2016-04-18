2b5b67e706ca92b4d101ef49a0bcfcd11b237153
TheJP <jp_55555@hotmail.com>
2016-04-18 15:39:33 +0200

    Implemented camera movement with mouse

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour
 {
-    public float cameraSpeed;
+    public float cameraSpeedKeyboard;
+    public float cameraSpeedMouse;
     public float rotation;
+
+    /// <summary>
+    /// Defines how wide / high the border is in pixels
+    /// When the mouse is in this border, the camera is moved.
+    /// </summary>
+    public Vector2 borderSize;
 
     void Update()
     {
-        //Camera movement
+        //Camera movement (keyboard)
         var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        transform.position += Time.deltaTime * cameraSpeed * (Quaternion.AngleAxis(rotation, Vector3.up) * direction);
+        var hasKeyboardInput = direction.sqrMagnitude > 0;
+        //Camera movement (mouse)
+        if (!hasKeyboardInput)
+        {
+            direction = new Vector3(
+                Input.mousePosition.x <= borderSize.x ? -1 : (Input.mousePosition.x >= Screen.width - borderSize.x ? 1 : 0), 0,
+                Input.mousePosition.y <= borderSize.y ? -1 : (Input.mousePosition.y >= Screen.height - borderSize.y ? 1 : 0)
+            );
+        }
+        if (direction.sqrMagnitude > 0)
+        {
+            var speed = Time.deltaTime * (hasKeyboardInput ? cameraSpeedKeyboard : cameraSpeedMouse);
+            transform.position += speed * (Quaternion.AngleAxis(rotation, Vector3.up) * direction.normalized);
+        }
     }
 }
