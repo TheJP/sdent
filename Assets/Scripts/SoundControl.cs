ecf226f2ba330bdb9d62c670029f9017f2b3e0d9
TheJP <jp_55555@hotmail.com>
2016-06-13 01:33:20 +0200

    Added ambient music to the game

---
+++ Assets/Scripts/SoundControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControl : MonoBehaviour
+{
+    public AudioClip[] ambientMusicTracks;
+
+    void Update()
+    {
+        //Play random songs
+        var audio = GetComponent<AudioSource>();
+        if (!audio.isPlaying || Input.GetButtonDown("NextSong"))
+        {
+            audio.clip = ambientMusicTracks[Random.Range(0, ambientMusicTracks.Length)];
+            audio.Play();
+        }
+
+        //Mute / Unmute audio
+        if (Input.GetButtonDown("Mute"))
+        {
+            AudioListener.pause = !AudioListener.pause;
+        }
+    }
+}
